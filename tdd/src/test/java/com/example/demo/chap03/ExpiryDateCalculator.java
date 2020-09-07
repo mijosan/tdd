d4a54d318cd24fdb7e9ca611a52fed1df72d1d0e
ChoiTaesan <mijosan@naver.com>
2020-09-07 21:59:18 +0900

    75Page

---
+++ tdd/src/test/java/com/example/demo/chap03/ExpiryDateCalculator.java
@@ -0,0 +1,11 @@
+package com.example.demo.chap03;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+	
+	public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+		return billingDate.plusMonths(1);
+	}
+	
+}
---
+++ tdd/src/test/java/com/example/demo/chap03/ExpiryDateCalculatorTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.chap03;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+class ExpiryDateCalculatorTest {
+
+	@Test
+	void 만원_납부하면_한달_뒤가_만료일이_됨() {
+		assertExpiryDate(LocalDate.of(2019, 3, 1), 10_000, LocalDate.of(2019, 4, 1));
+		
+		assertExpiryDate(LocalDate.of(2019, 5, 5), 10_000, LocalDate.of(2019, 6, 5));
+	}
+	
+	@Test
+	void 납부일과_한달_뒤_일자가_같지_않음() {
+		assertExpiryDate(LocalDate.of(2019, 1, 31), 10_000, LocalDate.of(2019, 2, 28));
+		assertExpiryDate(LocalDate.of(2019, 5, 31), 10_000, LocalDate.of(2019, 6, 30));
+		assertExpiryDate(LocalDate.of(2019, 1, 30), 10_000, LocalDate.of(2019, 2, 28));
+	}
+
+	private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+		ExpiryDateCalculator cal = new ExpiryDateCalculator();
+		LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+		assertEquals(expectedExpiryDate, realExpiryDate);
+	}
+	
+}
